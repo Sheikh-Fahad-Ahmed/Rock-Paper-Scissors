3a4d1e281d9c8f4912bac5c4256afdaaf932ab05
Fahad Ahmed <fahad838@gmail.com>
2024-08-05 18:09:52 +0530

    Finish building logic of the game
    Create a function playRound and coded in the logic
    for the game
    
    Each time a player won incremented its score by 1
    and whenever a player loses increment the
    computer score by 1

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function getComputerChoice() {
     randomNumber = Math.floor(Math.random() * 3) + 1;
     if (randomNumber == 1) {
-        return 'Rock';
+        return 'rock';
     } else if (randomNumber == 2) {
-        return 'Paper';
+        return 'paper';
     } else {
-        return 'Scissors';
+        return 'scissors';
     }
 }
 
@@ -14,5 +14,46 @@ function getHumanChoice() {
     return userChoice;
 }
 
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice == 'rock') {
+        if (computerChoice == 'paper') {
+            console.log("You lose! Paper beats Rock.");
+            computerScore += 1;
+        } else if (computerChoice == 'scissors') {
+            console.log("You Win! Rock beats Scissors.");
+            humanScore += 1;
+        } else {
+            console.log("Its a Draw!");
+        }
+    } else if (humanChoice == 'paper') {
+        if (computerChoice == 'rock') {
+            console.log("You Win! Paper Beats Rock.");
+            humanScore += 1;
+        } else if (computerChoice == 'scissors') {
+            console.log("You Lose! Scissors beat Paper.");
+            computerScore += 1;
+        } else {
+            console.log("Its a Draw!");
+        }
+    } else {
+        if (computerChoice == 'rock') {
+            console.log("You Lose! Rock beats scissors");
+            computerScore += 1;
+        } else if (computerChoice == 'paper') {
+            console.log("You win! Scissors beat Paper");
+            humanScore += 1;
+        }
+    }
+}
+
 let humanScore = 0;
-let computerScore =0 ;
+let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+console.log(`your score : ${humanScore}
+Computer Score : ${computerScore}`);
